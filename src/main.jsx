3b85186b7227f5ab6b71415549ea4760b2014da4
Gaurav Singh <gauravsingh9444@gmail.com>
2026-04-04 11:44:21 +0530

    Update main.jsx
    update the import from book page - components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import BookPage from "./pages/BookPage";
+import BookPage from "./components/pages/BookPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
