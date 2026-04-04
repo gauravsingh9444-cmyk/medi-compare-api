d069fff5670241cb8653a9155957d79ba725fc22
Gaurav Singh <gauravsingh9444@gmail.com>
2026-04-04 10:58:48 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import App from "./App";
+import BookPage from "./pages/BookPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/book" element={<BookPage />} />
+    </Routes>
+  </BrowserRouter>
 );
