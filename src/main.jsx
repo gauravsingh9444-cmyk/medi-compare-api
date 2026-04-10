d29738dbff732d86b103033076d2dc2b1f51aa19
Gaurav Singh <gauravsingh9444@gmail.com>
2026-04-10 12:00:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css"; // 🔥 THIS LINE FIXES YOUR UI
+
 import BookPage from "./components/pages/BookPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
